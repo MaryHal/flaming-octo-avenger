d6f055a893d922c777176409b967071bcff6089f
ioCx <sanfordlam9@gmail.com>
2015-05-12 18:18:35 -0700

    Draw model hitboxes.

--- barrage/test13.lua
+++ barrage/test13.lua
@@ -1,11 +1,36 @@
+local function bind(f,...)
+   local args = {...}
+   return function(...)
+      return f(unpack(args),...)
+   end
+end
+
 local theta = 0
 
 local test13
 test13 = {
    main = function()
-      launch(1, theta, 7, vanish)
-      launch(2, -theta, 7, vanish)
-      theta = theta + 7
+      setPosition(320, 240)
+
+      launch(1,  theta + 00, 3, test13.doSomething)
+      launch(2, -theta + 10, 6, test13.doSomething)
+
+      launch(1,  theta + 120, 3, test13.doSomething)
+      launch(2, -theta + 120, 6, test13.doSomething)
+
+      launch(1,  theta + 240, 3, test13.doSomething)
+      launch(2, -theta + 240, 6, test13.doSomething)
+
+      theta = theta + 6
+   end,
+
+   doSomething = function()
+      local turn = getTurn()
+      setSpeedRelative(0.02)
+
+      if (turn == 10) then
+         vanish()
+      end
    end
 }
 
